078faf7ec2b8060a0e596c182bd0d9fa6df10559
Jennie <thaoluu.310@gmail.com>
2022-11-28 15:59:35 +0700

    Finish BT

--- SeleniumTestNG/src/test/java/Bai10/AddProduct.java
+++ SeleniumTestNG/src/test/java/Bai10/AddProduct.java
@@ -1,6 +1,7 @@
 package Bai10;
 
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -17,18 +18,22 @@ public class AddProduct extends BaseTest{
         sleep(1000);
         findElement("//input[@placeholder='Product Name']").sendKeys("Lipstick new");
         findElement("//button[@title='Women Clothing & Fashion']").click();
-        findElement("div[class='dropdown-menu show'] input[aria-label='Search']").sendKeys(categoryName, Keys.ENTER);
+        findElement("//div[@class='dropdown-menu show']//div[@class='bs-searchbox']/input").sendKeys(categoryName, Keys.ENTER);
+        sleep(1000);
         findElement("//button[@title='Select Brand']").click();
-        findElement("div[class='dropdown-menu show'] input[aria-label='Search']").sendKeys("Loreal", Keys.ENTER);
+        findElement("//div[@class='dropdown-menu show']//div[@class='bs-searchbox']/input").sendKeys("Loreal", Keys.ENTER);
         sleep(1000);
         findElement("//input[@placeholder='Unit (e.g. KG, Pc etc)']").sendKeys("pc");
         findElement("//input[@name='min_qty']").clear();
         findElement("//input[@name='min_qty']").sendKeys("2");
+        sleep(1000);
         findElement("//tags[@role='tagslist']/span").sendKeys("Cosmetic", Keys.ENTER);
         findElement("(//div[@class='form-control file-amount'])[1]").click();
         findElement("//input[@placeholder='Search your files']").sendKeys("beauty");
         sleep(2000);
         findElement("//div[@title='beauty.png']").click();
+        findElement("//button[normalize-space()='Add Files']").click();
+        sleep(1000);
         findElement("//div[@class='col-md-1']//span").click();
         sleep(1000);
         findElement("(//button[@title='Nothing selected'])[1]").click();
@@ -42,13 +47,16 @@ public class AddProduct extends BaseTest{
         sleep(1000);
         findElement("//input[@placeholder='Unit price']").clear();
         findElement("//input[@placeholder='Unit price']").sendKeys("10");
+        sleep(1000);
         findElement("//input[@placeholder='Discount']").clear();
         findElement("//input[@placeholder='Discount']").sendKeys("10");
         findElement("//div[@class='col-md-3']//button[@title='Flat']").click();
         findElement("//ul[@class='dropdown-menu inner show']//a[@role='option']//span[text()='Percent']").click();
-        findElement("//input[@placeholder='Quantity']").clear();
-        findElement("//input[@placeholder='Quantity']").sendKeys("20");
+        sleep(1000);
+        Actions action = new Actions(driver);
+        action.moveToElement(findElement("//button[normalize-space()='Save & Unpublish']")).build().perform();
         findElement("//button[normalize-space()='Save & Unpublish']").click();
+        sleep(1000);
 
     }
 }
