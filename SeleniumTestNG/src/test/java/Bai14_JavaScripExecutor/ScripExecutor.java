539c615f6ce706d81057617d435173c766e31a79
thaolp <thaoluu.310@gmail.com>
2022-12-16 00:59:26 +0700

    Update waitEx

--- SeleniumTestNG/src/test/java/Bai14_JavaScripExecutor/ScripExecutor.java
+++ SeleniumTestNG/src/test/java/Bai14_JavaScripExecutor/ScripExecutor.java
@@ -4,6 +4,7 @@ import Bai10.BaseTest;
 import Keywords.WebUI;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -63,15 +64,25 @@ public class ScripExecutor extends BaseTest {
         driver.findElement(By.xpath("//span[normalize-space()='Products']")).click();
 
         //Khai báo WebDriverWait = Explicit Wait
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
         //Chờ đợi menu Brand đến khi sẵn sàng hiển thị để có thể thao tác được
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[normalize-space()='Brand']")));
-
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[normalize-space()='Brand']")));
+        WebUI.waitForElementVisible(driver, By.xpath("//span[normalize-space()='Brand']"));
         driver.findElement(By.xpath("//span[normalize-space()='Brand']")).click();
 
         String headerAddNewBrand = driver.findElement(By.xpath("//div[@class='card-header']/h5")).getText();
         Assert.assertEquals(headerAddNewBrand, "Add New Brand");
+
+        driver.findElement(By.xpath("//input[@id='search']")).sendKeys("Demo brand1", Keys.ENTER);
+
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tbody/tr/td[2]")));
+
+        WebUI.waitForElementVisible(driver, By.xpath("//tbody/tr/td[2]"));
+
+        String brandName = driver.findElement(By.xpath("//tbody/tr/td[2]")).getText();
+        System.out.println(brandName);
+        Assert.assertEquals(brandName, "Demo brand1");
     }
 
 }
--- SeleniumTestNG/src/test/java/Keywords/WebUI.java
+++ SeleniumTestNG/src/test/java/Keywords/WebUI.java
@@ -1,5 +1,13 @@
 package Keywords;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class WebUI {
     public static void sleep (double seconds) {
         try {
@@ -7,5 +15,10 @@ public class WebUI {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void waitForElementVisible (WebDriver driver, By by){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
     }
 }
