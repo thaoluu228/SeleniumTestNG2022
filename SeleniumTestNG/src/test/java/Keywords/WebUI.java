5bfca8ba9d0922b999e0f958d2b0c4cf768bcdf5
thaolp <thaoluu.310@gmail.com>
2022-12-20 12:57:00 +0700

    Update file

--- SeleniumTestNG/src/test/java/Keywords/WebUI.java
+++ SeleniumTestNG/src/test/java/Keywords/WebUI.java
@@ -1,12 +1,15 @@
 package Keywords;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class WebUI {
     public static void sleep (double seconds) {
@@ -21,4 +24,41 @@ public class WebUI {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
     }
+
+    public static void waitForElementPresent (WebDriver driver, By by){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    public static void waitForElementClickable (WebDriver driver, By by){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.elementToBeClickable(by));
+    }
+
+    public static Boolean checkElementExist(WebDriver driver, String xpath) {
+        List<WebElement> listElement = driver.findElements(By.xpath(xpath));
+
+        if (listElement.size() > 0) {
+            System.out.println("Element " + xpath + " existing.");
+            return true;
+        } else {
+            System.out.println("Element " + xpath + " NOT exist.");
+            return false;
+        }
+    }
+
+    public static void waitForPageLoaded(WebDriver driver) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30), Duration.ofMillis(500));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        //Wait for Javascript to load
+        ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver driver) {
+                return js.executeScript("return document.readyState").toString().equals("complete");
+            }
+        };
+    }
+
+
 }
